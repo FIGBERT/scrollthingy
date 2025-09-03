adeb5ee102df4a2006d92bc4a3449cf54d4284d0
FIGBERT <figbert@figbert.com>
2025-09-03 17:22:42 -0400

    Simplify line breaks

--- server/internal/server/livekit.go
+++ server/internal/server/livekit.go
@@ -3,7 +3,6 @@ package server
 import (
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/pion/webrtc/v4"
@@ -17,14 +16,8 @@ const ROOM_NAME = "default"
 func (s *Server) token() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		at := auth.NewAccessToken(os.Getenv("LIVEKIT_API_KEY"), os.Getenv("LIVEKIT_API_SECRET"))
-		grant := &auth.VideoGrant{
-			RoomJoin: true,
-			Room:     ROOM_NAME,
-		}
-		at.
-			SetVideoGrant(grant).
-			SetValidFor(time.Hour).
-			SetIdentity(uuid.NewString())
+		grant := &auth.VideoGrant{RoomJoin: true, Room: ROOM_NAME}
+		at.SetVideoGrant(grant).SetIdentity(uuid.NewString())
 
 		token, err := at.ToJWT()
 		if err != nil {
